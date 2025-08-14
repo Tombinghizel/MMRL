fd396066034a761032cfd5ca1a977de857c7163a
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2025-08-14 15:57:08 +0200

    Add build info section to Developer settings screen
    Added a new section in the Developer settings screen displaying the latest commit ID, build tools version, and compile SDK using BuildConfig values. Also refactored the layout to group related settings into sections and updated strings.xml with new string resources.

--- app/src/main/kotlin/com/dergoogler/mmrl/ui/screens/settings/developer/DeveloperScreen.kt
+++ app/src/main/kotlin/com/dergoogler/mmrl/ui/screens/settings/developer/DeveloperScreen.kt
@@ -1,10 +1,14 @@
 package com.dergoogler.mmrl.ui.screens.settings.developer
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.dergoogler.mmrl.BuildConfig
 import com.dergoogler.mmrl.R
 import com.dergoogler.mmrl.ui.component.SettingsScaffold
 import com.dergoogler.mmrl.ui.component.listItem.dsl.ListItemScope
 import com.dergoogler.mmrl.ui.component.listItem.dsl.ListScope
+import com.dergoogler.mmrl.ui.component.listItem.dsl.component.Item
+import com.dergoogler.mmrl.ui.component.listItem.dsl.component.Section
 import com.dergoogler.mmrl.ui.component.listItem.dsl.component.SwitchItem
 import com.dergoogler.mmrl.ui.component.listItem.dsl.component.item.Description
 import com.dergoogler.mmrl.ui.component.listItem.dsl.component.item.Title
@@ -36,19 +40,38 @@ fun DeveloperScreen() {
     SettingsScaffold(
         title = R.string.settings_developer
     ) {
-        SwitchItem(
-            checked = userPreferences.developerMode,
-            onChange = viewModel::setDeveloperMode,
-        ) {
-            Title(R.string.settings_developer_mode)
-            Description(R.string.settings_developer_mode_desc)
+        Section {
+            SwitchItem(
+                checked = userPreferences.developerMode,
+                onChange = viewModel::setDeveloperMode,
+            ) {
+                Title(R.string.settings_developer_mode)
+                Description(R.string.settings_developer_mode_desc)
+            }
+
+            DeveloperSwitch(
+                checked = userPreferences.devAlwaysShowUpdateAlert,
+                onChange = viewModel::setDevAlwaysShowUpdateAlert,
+            ) {
+                Title(R.string.settings_always_show_update_alert)
+            }
         }
 
-        DeveloperSwitch(
-            checked = userPreferences.devAlwaysShowUpdateAlert,
-            onChange = viewModel::setDevAlwaysShowUpdateAlert,
+        Section(
+            divider = false
         ) {
-            Title(R.string.settings_always_show_update_alert)
+            Item {
+                Title(stringResource(R.string.latest_commit_id))
+                Description(BuildConfig.LATEST_COMMIT_ID)
+            }
+            Item {
+                Title(stringResource(R.string.build_tools_version))
+                Description(BuildConfig.BUILD_TOOLS_VERSION)
+            }
+            Item {
+                Title(stringResource(R.string.compile_sdk))
+                Description(BuildConfig.COMPILE_SDK)
+            }
         }
     }
 }
